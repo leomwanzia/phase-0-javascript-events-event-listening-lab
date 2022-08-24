330c619a2bc0e1774a5c901a226ccf4be6f66e5b
Leo <leomutungi19@gmail.com>
2022-08-24 12:53:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+// let input;
+// input = document.getElementById("button");
+// function addingEventListener() {
+//     const input = document.getElementsById("button");
+
+// 	function clickAlert() {
+// 		alert("I was clicked!");
+// 	}
+
+// 	input.addingEventListener("click", clickAlert);
+// }
+
+let input;
+input = document.getElementById("button");
 function addingEventListener() {
+    const input = document.getElementById("button");
+
+    function clickAlert() {
+        alert("I was clicked!");
+    }
+
+    input.addEventListener("click", clickAlert);
 }
