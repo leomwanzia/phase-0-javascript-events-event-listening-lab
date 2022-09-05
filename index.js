75fee444c1a868379279cbd97b3acbb01ef7cc08
Leo <leomutungi19@gmail.com>
2022-09-06 00:37:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-// let input;
-// input = document.getElementById("button");
-// function addingEventListener() {
-//     const input = document.getElementsById("button");
-
-// 	function clickAlert() {
-// 		alert("I was clicked!");
-// 	}
-
-// 	input.addingEventListener("click", clickAlert);
-// }
-
-let input;
-document.getElementById("button").addEventListener
-('click', () => alert('I was clicked!') )
 
 function addingEventListener() {
-const input = document.getElementById("button");
-if (input){
-    input.addEventListener('click', clickAlert);
-}
+const input = document.getElementById('button');
+    if (input){
+        input.addEventListener('click', clickAlert);
+    }
 function clickAlert() {
     alert('I was clicked!');
     }
-    
+     
     input.addEventListener('click', clickAlert);
 } 
